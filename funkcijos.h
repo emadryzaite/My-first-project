8955540c26dae98437a67c1f4eb465ed34ffd879
emadryzaite <113093671+emadryzaite@users.noreply.github.com>
2022-10-13 22:46:13 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,54 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <string>
 #include <algorithm>
-#include <numeric>
-#include <stdio.h>     
-#include <stdlib.h>    
-#include <time.h> 
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
 #include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <vector>
+#include <chrono>
 
-using std::cout;
+static std::chrono::steady_clock::time_point pradzia;
+
 using std::cin;
+using std::cout;
 using std::endl;
-using std::string; 
-using std::stoi;
-using std::setw;
-using std::setprecision;
-using std::sort;
-using std::vector;
-using std::left;
 using std::fixed;
 using std::ifstream;
-using std::stringstream; 
+using std::left;
+using std::setprecision;
+using std::setw;
+using std::sort;
+using std::stoi;
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::ofstream;
+using std::to_string;
 
-struct irasas
-{
+struct irasas {
   string vardas;
   string pavarde;
-  vector <int> paz;
+  vector<int> paz;
   int egzas;
   float galut;
 };
 
-void nuskaitymas(vector <irasas> &Stud);
+void nuskaitymas(vector<irasas> &Stud, string failas);
 bool vardInfo(string skai);
 string vardIrasymas(string irasymas);
 bool skaiInfo(string pazym);
 bool skInfo(string pazym);
 int skIrasymas(string irasymas, bool);
-int suma(vector <int> paz);
-float vidurkis(vector <int> paz);
-float mediana(vector <int> paz);
-void vidMed(vector <irasas> &Stud);
+int suma(vector<int> paz);
+float vidurkis(vector<int> paz);
+float mediana(vector<int> paz);
+void vidMed(vector<irasas> &Stud);
 float galutinis(float, int egzas);
 int atsitiktiniai();
-int pavardTvarka(vector <irasas> Stud);
-int vardTvarka(vector <irasas> Stud);
-void spausdinimas(vector <irasas> Stud);
-void papildoma(vector <irasas> &Stud);
+int pavardTvarka(vector<irasas> Stud);
+int vardTvarka(vector<irasas> Stud);
+void spausdinimas(vector<irasas> Stud, string failas);
+void papildoma(vector<irasas> &Stud);
 bool patvirtinimas();
-void rikiavimas (vector <irasas> &Stud);
+void rikiavimas(vector<irasas> &Stud);
 bool pavardLyginimas(irasas &a, irasas &b);
+void generavimas(int sk, string &failas);
+int pasirinkimas();
+int skIrasymas();
+bool skGenTikrinimas(string pazym);
+void skirstymas(vector <irasas> Stud, vector <irasas> &Vargsiukai, vector <irasas> &Kietiakai);
