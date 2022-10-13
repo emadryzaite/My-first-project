4cde56db2dbe8a9b21ce512e369145e0f27cc0f1
emadryzaite <113093671+emadryzaite@users.noreply.github.com>
2022-10-13 22:46:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include "funkcijos.h"
 
-int main() 
-{
-  vector <irasas> Stud;
+int main() {
+  vector<irasas> Stud;
+  vector<irasas> Vargsiukai;
+  vector<irasas> Kietiakai;
+  string failas = "";
   srand(time(0));
-  cout << "Ar norite duomenis irasysite patys? (iveskite + jei norite, - jei ne) ";
-  if(patvirtinimas()) papildoma(Stud);
-  else nuskaitymas(Stud);
+
+  cout << "Ar norite duomenis ivesti patys (kitu atveju duomenys bus "
+          "nuskaityti is failo)? (+/-) ";
+  if (patvirtinimas())
+    papildoma(Stud);
+  else{
+  generavimas(pasirinkimas(), failas);
+  nuskaitymas(Stud, failas);
+  }
   vidMed(Stud);
-  spausdinimas(Stud);
+  skirstymas(Stud, Vargsiukai, Kietiakai);
+  spausdinimas(Vargsiukai, "vargsiukai.txt");
+  spausdinimas(Kietiakai, "kietiakai.txt");
   Stud.clear();
+  Vargsiukai.clear();
+  Kietiakai.clear();
 }
