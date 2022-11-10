6973c2f90f12b0945a686d2119f7e7e6b19c9be4
emadryzaite <113093671+emadryzaite@users.noreply.github.com>
2022-11-10 13:45:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 int main() {
   vector<irasas> Stud;
+  vector<irasas> Vargsiukai;
   vector<irasas> Kietiakai;
   string failas = "";
   srand(time(0));
 
   cout << "Ar norite duomenis ivesti patys (kitu atveju duomenys bus "
-          "nuskaityti is failo)? (+/-) ";
+          "nuskaityti is failo) ? (+/-) ";
   if (patvirtinimas())
     papildoma(Stud);
   else{
@@ -15,9 +16,24 @@ int main() {
   nuskaitymas(Stud, failas);
   }
   vidMed(Stud);
-  skirstymas(Stud, Kietiakai);
-  spausdinimas(Stud, "vargsiukai.txt");
-  spausdinimas(Kietiakai, "kietiakai.txt");
+  cout << "ar norite studentu rusiavima vykdyti 1 strategija, kitu atveju 2(+/-) ";
+  if(patvirtinimas()) {
+    skirstymas1(Stud,Vargsiukai, Kietiakai);
+    spausdinimas(Vargsiukai, "vargsiukai.txt");
+    spausdinimas(Kietiakai, "kietiakai.txt");
+  }
+  else {
+    cout << "Ar norite naudoti optimizuota strategija, jei ne bus naudojama neoptimizuota (+/-)";
+    if(patvirtinimas()) 
+    skirstymas2(Stud, Kietiakai);
+    else 
+    skirstymas3(Stud, Kietiakai);
+    spausdinimas(Stud, "vargsiukai.txt");
+    spausdinimas(Kietiakai, "kietiakai.txt");
+  }
+  
+
   Stud.clear();
+  Vargsiukai.clear();
   Kietiakai.clear();
 }
